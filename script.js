4b69db4c3408c785e7aa5c293e8bbacfa4b7f274
BenBonno <benoit.bonnoron@gmail.com>
2022-09-21 17:50:12 +0200

    Second commit

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const playerName = prompt("What's your name ?")
-const random = Math.floor(Math.random() * 100) + 1;
+const random = Math.round(Math.floor(Math.random() * 100) + 1);
 let answer
 
 do {
-    answer = parseInt(prompt("Give me a number between 1 and 100", "0"), 10);
+    answer = parseInt(prompt("Give me a number between 1 and 100", "1<number<100"));
     if (isNaN(answer)) {
-        alert("I said a number noob");
+        alert("I said a number");
     }
-
+    if (answer < 1 || answer > 100) {
+        alert("Out of range")
+    }
 }
 while (isNaN(answer));
 
