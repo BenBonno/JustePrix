98016f9ceedde41c9b97d241948b186df2507a8e
BenBonno <benoit.bonnoron@gmail.com>
2022-09-22 10:25:42 +0200

    Test Boolean

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
 const playerName = prompt("What's your name ?")
 const random = Math.round(Math.floor(Math.random() * 100) + 1);
 let answer = "";
-let outOfRange = "";
+let outOfRange;
 console.log(random);
 do {
-    outOfRange = 0;
-    answer = parseInt(prompt("Give me a integer number between 1 and 100", "1<number<100"));
+    outOfRange = false;
+    answer = parseInt(prompt("Give me a integer number between 1 and 100"));
 
     if (isNaN(answer)) {
         alert("I said a number");
     }
     if (answer < 1 || answer > 100) {
         alert("Out of range")
-        outOfRange++
+        outOfRange = true;
     }
     if (answer > 1 || answer < 100) {
         answer = answer
     }
-
 }
+while (isNaN(answer) || outOfRange === false);
 
-while (isNaN(answer) || outOfRange === 1);
-console.log(answer);
 do {
-
     if (answer < random) {
         answer = parseInt(prompt("It is more"));
     }
     else if (answer > random) {
         answer = parseInt(prompt("It is less"));
     }
-
 }
 while (!isNaN(answer) && answer !== random);
 
