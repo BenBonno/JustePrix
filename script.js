9d6e61c504acf1059a99d202820e7ca28da017e3
BenBonno <benoit.bonnoron@gmail.com>
2022-09-22 09:27:36 +0200

    Mostly done

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const playerName = prompt("What's your name ?")
 const random = Math.round(Math.floor(Math.random() * 100) + 1);
-let answer
-
+let answer = "";
+let outOfRange = "";
+console.log(random);
 do {
-    answer = parseInt(prompt("Give me a number between 1 and 100", "1<number<100"));
+    outOfRange = 0;
+    answer = parseInt(prompt("Give me a integer number between 1 and 100", "1<number<100"));
+
     if (isNaN(answer)) {
         alert("I said a number");
     }
     if (answer < 1 || answer > 100) {
         alert("Out of range")
+        outOfRange++
     }
+    if (answer > 1 || answer < 100) {
+        answer = answer
+    }
+
 }
-while (isNaN(answer));
+
+while (isNaN(answer) || outOfRange === 1);
+console.log(answer);
+do {
+    if (answer < random) {
+        answer = prompt("It is more");
+    }
+    if (answer > random) {
+        answer = prompt("It is less");
+    }
+
+}
+while (answer !== random)
+prompt(`Well done !${playerName}`);
 
